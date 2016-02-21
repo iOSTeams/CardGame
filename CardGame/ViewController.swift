a51b0f0e52c4b90ad1455c0847b3e41f21132ac1
King Justin <kingjustin@tests-MBP-2.att.net>
2016-02-21 12:45:59 -0800

    Containers over cards and one for button

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var PlayUIButton: UIButton!
-    @IBOutlet weak var Card1ImageView: UIImageView!
-    @IBOutlet weak var Card2ImageView: UIImageView!
+    @IBOutlet weak var roundUIButton: UIButton!
+    @IBOutlet weak var secondCardView: UIImageView!
+    @IBOutlet weak var firstCardView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +23,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func roundUIButtonTapped(sender: UIButton) {
+        
+        let one = "hello"
+        
+        let two = "bye"
+        
+        if (self.roundUIButton.titleLabel != one) {
+                    self.roundUIButton.setTitle(two, forState: UIControlState.Normal)
+            self.firstCardView.addMotionEffect(<#T##effect: UIMotionEffect##UIMotionEffect#>)
+            
+        }
+        else if (self.roundUIButton.titleLabel == two) {
+            
+            self.roundUIButton.setTitle(one, forState: UIControlState.Normal)
+            
+        }
+
+    }
 
 }
 
